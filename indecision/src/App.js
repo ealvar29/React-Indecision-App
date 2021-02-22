de68e23d4f7650cbaa6430c7343abd42b0092f5b
Eduardo <edalvarez2015@gmail.com>
2021-02-22 15:24:09 -0600

    Indecision app initialized along with tailwind css

---
+++ indecision/src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <button class="px-4 py-1 text-md bg-white text-red-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+          Message
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
