161597175177bf71183c272c643eef8b079f3e56
Eduardo <edalvarez2015@gmail.com>
2021-02-22 21:20:52 -0600

    adding nested components and styling from Tailwind!

--- indecision/src/App.js
+++ indecision/src/App.js
@@ -1,28 +1,73 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button class="px-4 py-1 text-md bg-white text-red-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-          Message
-        </button>
-      </header>
+    <div className="container block">
+      <Header />
+      <Action />
+      <Option />
+      <Options />
+      <AddOption />
     </div>
   );
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Indecision</h1>
+        <h2>Put your life in the hands of a computer.</h2>
+      </div>
+    );
+  }
+}
+
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          What should I do?
+        </button>
+      </div>
+    );
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Option component here</p>
+      </div>
+    );
+  }
+}
+
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Options component here</p>
+      </div>
+    );
+  }
+}
+
+class AddOption extends React.Component {
+  render() {
+    return (
+      <form>
+        <input
+          className="box-content border-gray-600"
+          placeholder="Add Option"
+        />
+      </form>
+    );
+  }
 }
 
 export default App;
